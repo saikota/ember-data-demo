c1e1ab7f5840869cc3aa5785eff8680f342b03a9
Ravali <ravali@Ravalis-MacBook-Pro.local>
2019-07-04 18:30:26 -0700

    posts/show/1

---
+++ app/routes/posts/show.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model(params){
+    return this.store.findRecord('post',params.id);
+  },
+  setupController(controller,model){
+    Ember.set(controller,'post',model);
+  }
+});
